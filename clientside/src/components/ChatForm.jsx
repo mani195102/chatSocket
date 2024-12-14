8f482ee22c873a95e4905a48a8abf27081b59126
Manimuthu <86956699+mani195102@users.noreply.github.com>
2024-12-14 16:30:55 +0530

    ok

--- clientside/src/components/ChatForm.jsx
+++ clientside/src/components/ChatForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 import { Box, TextField, Button, MenuItem, Select, InputLabel, FormControl } from "@mui/material";
 
 const ChatForm = ({ onSend, setReceiver }) => {
